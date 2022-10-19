96313ab2b783a83a289f96bb3107a3f912cc081f
Jan Černý <cernyj87@fit.cvut.cz>
2022-10-20 01:47:08 +0200

    Remove TODO comments

--- hw-04/src/Iterator/AbstractOrderIterator.php
+++ hw-04/src/Iterator/AbstractOrderIterator.php
@@ -6,7 +6,6 @@ use Node;
 
 abstract class AbstractOrderIterator implements \Iterator
 {
-    // TODO: shared attributes?
     protected Node $rootNode;
     protected ?Node $currentNode;
 
@@ -14,31 +13,23 @@ abstract class AbstractOrderIterator implements \Iterator
 
     public function __construct(Node $root)
     {
-        // TODO: Implement constructor.
         $this->rootNode = $root;
-        //$this->buildStack();
     }
 
     public function current(): ?Node
     {
-        // TODO: Implement current() method.
         return $this->currentNode;
     }
 
     public abstract function next(): void;
-    //{
-        // TODO: Implement next() method.
-    //}
 
     public function key(): bool|int|float|string|null
     {
-        // TODO: Implement key() method.
         return $this->current()->getValue();
     }
 
     public function valid(): bool
     {
-        // TODO: Implement valid() method.
         return $this->current() !== null;
     }
 
