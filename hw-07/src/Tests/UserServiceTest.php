acf8f20247bf912564f503b3145c89e24234b9bb
Jan Černý <cernyj87@fit.cvut.cz>
2022-11-03 21:46:45 +0100

    Use static variables

--- hw-07/src/Tests/UserServiceTest.php
+++ hw-07/src/Tests/UserServiceTest.php
@@ -8,15 +8,15 @@ use PHPUnit\Framework\TestCase;
 
 class UserServiceTest extends TestCase
 {
-    private string $username = "John";
-    private string $email = "john@example.com";
-    private string $json;
+    private static string $username = "John";
+    private static string $email = "john@example.com";
+    private static string $json;
 
-    protected function setUp(): void
+    public static function setUpBeforeClass(): void
     {
-        $this->json = json_encode([
-            'username' => $this->username,
-            'email' => $this->email
+        self::$json = json_encode([
+            'username' => self::$username,
+            'email' => self::$email
         ], JSON_THROW_ON_ERROR);
     }
 
@@ -26,13 +26,13 @@ class UserServiceTest extends TestCase
         $mockStorage = $this->createMock(Storage::class);
         $mockStorage->expects($this->once())
                     ->method("save")
-                    ->with($this->anything(), $this->json);
+                    ->with($this->anything(), self::$json);
 
         // Create UserService
         $userService = new UserService($mockStorage);
 
         // Test createUser()
-        $userService->createUser($this->username, $this->email);
+        $userService->createUser(self::$username, self::$email);
     }
 
     public function testGetUsernameReturnsUsername()
@@ -41,13 +41,13 @@ class UserServiceTest extends TestCase
         $mockStorage = $this->createMock(Storage::class);
         $mockStorage->expects($this->once())
                     ->method("get")
-                    ->willReturn($this->json);
+                    ->willReturn(self::$json);
 
         // Create UserService
         $userService = new UserService($mockStorage);
 
         // Test getUsername()
-        $this->assertEquals($this->username, $userService->getUsername(42));
+        $this->assertEquals(self::$username, $userService->getUsername(42));
     }
 
     public function testGetUsernameReturnsNull()
@@ -71,13 +71,13 @@ class UserServiceTest extends TestCase
         $mockStorage = $this->createMock(Storage::class);
         $mockStorage->expects($this->once())
             ->method("get")
-            ->willReturn($this->json);
+            ->willReturn(self::$json);
 
         // Create UserService
         $userService = new UserService($mockStorage);
 
         // Test getUsername()
-        $this->assertEquals($this->email, $userService->getEmail(42));
+        $this->assertEquals(self::$email, $userService->getEmail(42));
     }
 
     public function testGetEmailReturnsNull()
