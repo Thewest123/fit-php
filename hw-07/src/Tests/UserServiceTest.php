605557519493b5dfcee966cf160d905d1b1966e9
Jan Černý <cernyj87@fit.cvut.cz>
2022-11-03 21:40:04 +0100

    Add tests for UserService

--- hw-07/src/Tests/UserServiceTest.php
+++ hw-07/src/Tests/UserServiceTest.php
@@ -2,9 +2,96 @@
 
 namespace HW\Tests;
 
+use HW\Lib\Storage;
+use HW\Lib\UserService;
 use PHPUnit\Framework\TestCase;
 
 class UserServiceTest extends TestCase
 {
-    // TODO
+    private string $username = "John";
+    private string $email = "john@example.com";
+    private string $json;
+
+    protected function setUp(): void
+    {
+        $this->json = json_encode([
+            'username' => $this->username,
+            'email' => $this->email
+        ], JSON_THROW_ON_ERROR);
+    }
+
+    public function testCreateUserSaveCalled()
+    {
+        // Mock storage->save()
+        $mockStorage = $this->createMock(Storage::class);
+        $mockStorage->expects($this->once())
+                    ->method("save")
+                    ->with($this->anything(), $this->json);
+
+        // Create UserService
+        $userService = new UserService($mockStorage);
+
+        // Test createUser()
+        $userService->createUser($this->username, $this->email);
+    }
+
+    public function testGetUsernameReturnsUsername()
+    {
+        // Mock storage->get()
+        $mockStorage = $this->createMock(Storage::class);
+        $mockStorage->expects($this->once())
+                    ->method("get")
+                    ->willReturn($this->json);
+
+        // Create UserService
+        $userService = new UserService($mockStorage);
+
+        // Test getUsername()
+        $this->assertEquals($this->username, $userService->getUsername(42));
+    }
+
+    public function testGetUsernameReturnsNull()
+    {
+        // Mock storage->get()
+        $mockStorage = $this->createMock(Storage::class);
+        $mockStorage->expects($this->once())
+                    ->method("get")
+                    ->willReturn(null);
+
+        // Create UserService
+        $userService = new UserService($mockStorage);
+
+        // Test getUsername()
+        $this->assertEquals(null, $userService->getUsername(42));
+    }
+
+    public function testGetEmailReturnsEmail()
+    {
+        // Mock storage->get()
+        $mockStorage = $this->createMock(Storage::class);
+        $mockStorage->expects($this->once())
+            ->method("get")
+            ->willReturn($this->json);
+
+        // Create UserService
+        $userService = new UserService($mockStorage);
+
+        // Test getUsername()
+        $this->assertEquals($this->email, $userService->getEmail(42));
+    }
+
+    public function testGetEmailReturnsNull()
+    {
+        // Mock storage->get()
+        $mockStorage = $this->createMock(Storage::class);
+        $mockStorage->expects($this->once())
+            ->method("get")
+            ->willReturn(null);
+
+        // Create UserService
+        $userService = new UserService($mockStorage);
+
+        // Test getUsername()
+        $this->assertEquals(null, $userService->getEmail(42));
+    }
 }
